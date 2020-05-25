25ac2ab01956dbdeecf5cb94f700409b13b2bbdf
mayda-mac <mayda.werfali@gmail.com>
2020-05-25 15:02:29 +0200

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: 'Placeholder Store',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+        accentColor: Colors.deepOrangeAccent[100],
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: HomeScreen(),
+    );
+  }
+}
